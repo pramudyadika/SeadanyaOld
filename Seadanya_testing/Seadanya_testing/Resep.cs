bf0980ce2cecdb973e79b55cb13f5bd9ac6afbd3
rahmiyatulhasanah <rahmiyatulhasanah@mail.ugm.ac.id>
2022-09-14 14:16:52 +0700

    Upload .vs dan Seadanya_testing

---
+++ Seadanya_testing/Seadanya_testing/Resep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seadanya_testing
+{
+    internal class Resep
+    {
+        private string _nama;
+        private string[] _bahan;
+        private string[] _langkah;
+        private string _link;
+        private string[] _tags;
+    }
+}
